20ee258b10c72a68cfa51c1a8bff9efeb94dd3f6
Joel Lim <akaru@littlix.com>
2015-12-19 19:39:53 +0800

    Query Question 1

--- app/main.rb
+++ app/main.rb
@@ -1,32 +1,15 @@
 require_relative './models/congressperson.rb'
 
-# puts "Which state would you like to query about?"
-# input = gets.chomp
+puts "Which state would you like to query about?"
+input = gets.chomp
 
-# puts "Senators:"
-# x = Congressperson.where(title: 'Sen', state: input).order(:lastname)
-# x.each do |y|
-# puts "  " + y.firstname + " " + y.lastname + " (" + y.party + ")"
-# end
-# puts "Representatives:"
-# x = Congressperson.where(title: 'Rep', state: input).order(:lastname)
-# x.each do |y|
-# puts "  " + y.firstname + " " + y.lastname + " (" + y.party + ")"
-# end
-
-# puts "Which gender stats do you want to look at?"
-# input = gets.chomp
-# ------------------------
-puts "Gender Stats"
-x = Congressperson.where(gender: input, title: 'Sen', in_office: true)
-noofsenators = x.length
-x = Congressperson.where(gender: input, title: 'Rep', in_office: true)
-noofrepresentatives = x.length
-
-x = Congressperson.where(title: 'Sen', in_office: true)
-totalnoofsenators = x.length
-x = Congressperson.where(title: 'Rep', in_office: true)
-totalnoofrepresentatives = x.length
-
-puts input.to_s + " Senators: " + noofsenators.to_s + " (" + (noofsenators*100/totalnoofsenators).to_s + "%)"
-puts input.to_s + " Representatives: " + noofrepresentatives.to_s + " (" + (noofrepresentatives*100/totalnoofrepresentatives).to_s + "%)"
+puts "Senators:"
+x = Congressperson.where(title: 'Sen', state: input).order(:lastname)
+x.each do |y|
+puts "  " + y.firstname + " " + y.lastname + " (" + y.party + ")"
+end
+puts "Representatives:"
+x = Congressperson.where(title: 'Rep', state: input).order(:lastname)
+x.each do |y|
+puts "  " + y.firstname + " " + y.lastname + " (" + y.party + ")"
+end
