11fed4b155ec011c157a4cef29e1d03331b369f9
Joel Lim <akaru@littlix.com>
2015-12-19 19:37:53 +0800

    Query Question 1

--- app/main.rb
+++ app/main.rb
@@ -1,1 +1,15 @@
 require_relative './models/congressperson.rb'
+
+puts "Which state would you like to query about?"
+input = gets.chomp
+
+puts "Senators:"
+x = Congressperson.where(title: 'Sen', state: input).order(:lastname)
+x.each do |y|
+puts "  " + y.firstname + " " + y.lastname + " (" + y.party + ")"
+end
+puts "Representatives:"
+x = Congressperson.where(title: 'Rep', state: input).order(:lastname)
+x.each do |y|
+puts "  " + y.firstname + " " + y.lastname + " (" + y.party + ")"
+end
